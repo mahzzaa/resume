0059c65a7cace31a9ab6c2293d3cebfae70071c3
mahzza <mahsa.79mirmiran@gmail.com>
2020-07-23 23:46:30 +0430

    made an app

--- vue.config.js
+++ vue.config.js
@@ -11,4 +11,46 @@ module.exports = {
       },
     ]
   },
+
+  pwa: {
+    name      : 'resume',
+    short_name: 'resume',
+    themeColor: '#6ccf9e',
+    iconPath  : {
+      favicon32     : 'img/app_icons/icon-32x32.png',
+      favicon16     : 'img/app_icons/icon-16x16.png',
+      appleTouchIcon: 'img/app-icons/icon-152x152.png',
+      msTileImage   : 'img/app-icons/icon-144x144.png',
+    },
+    manifestOptions: {
+      background_color: '#7c2a95',
+      icons           : [
+        {
+          src  : 'img/app_icons/icon-192x192.png',
+          sizes: '192x192',
+          type : 'image/png',
+        },
+
+        {
+          src  : 'img/app_icons/icon-512x512.png',
+          sizes: '512x512',
+          type : 'image/png',
+        },
+
+        {
+          src    : 'img/app_icons/icon-192x192.png',
+          sizes  : '192x192',
+          type   : 'image/png',
+          purpose: 'maskable',
+        },
+
+        {
+          src    : 'img/app_icons/icon-512x512.png',
+          sizes  : '512x512',
+          type   : 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+  },
 }
